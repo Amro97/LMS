70b6681dbe60dac76688237a51742810973ec161
Amro97 <Amro97@users.noreply.github.com>
2021-06-05 19:52:02 +0300

    uploading the project

---
+++ LMS/src/Controller/LoginController.java
@@ -0,0 +1,36 @@
+package Controller;
+import Model.LoginModel;
+import View.Login;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginController implements ActionListener{
+	LoginModel model;
+	Login view;
+	
+	
+	//Constractor gets login object(view == login object)
+	public LoginController(Login view) {
+		this.model = new LoginModel();
+		this.view = view;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent caller) {
+		
+		
+		switch(caller.getActionCommand())
+				{
+				case("Login"):
+					model.LoginFunc(view);
+					break;
+					
+				}
+	}
+
+	
+	
+	
+}
